fa4222e11f0520785115a9ab6f78d5e21f8e28c0
chengzhongkai <chengzhongkai@zto.cn>
2020-04-25 20:35:25 +0800

    代理url问题

--- src/axios/api.js
+++ src/axios/api.js
@@ -3,12 +3,12 @@ import Axios from "./ajax";
 // https://api.abcyun.co/  pass: vison123
 // https://api.abcyun.co/api/tool/yiyan/token/5ea136920e1fa
 
+let base_url = "https://api.abcyun.co/api/tool/yiyan/token/5ea136920e1fa";
+if (process.env.NODE_ENV === "development") {
+  base_url = "/MARQUEE_URL";
+}
+
 // 获取语录
 export const marqueeInfo = params => {
-  return Axios.get(
-    "/MARQUEE_URL",
-    params
-  ).then(res => res.data);
+  return Axios.get(base_url, params).then(res => res.data);
 };
-
-
